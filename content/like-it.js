45dbbc85c987634698671f081709ac19f98160fb
Nikolay Yordanov <me@nyordanov.com>
2010-09-03 11:37:39 +0300

    add js code for the ajax request, currently no backend code

--- content/like-it.js
+++ content/like-it.js
@@ -1,1 +1,12 @@
- 
+(function($) {
+	$('.likeit-canvote .likeit-text').click(function() {
+		$.post(ajax_url, {
+			id: $(this).attr('id').split('_')[1],
+			action: 'likeit_register_vote'
+		}, function(data) {
+			$(this).parent().find('.likeit-count span').fadeIn(250, function() {
+				$(this).text(data);
+			});
+		})
+	});
+})(jQuery);
