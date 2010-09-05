2531b8a318816ef6b1b867846a786315c4b7c980
Nikolay Yordanov <me@nyordanov.com>
2010-09-05 14:07:03 +0300

    add config page option to change likeit-autodisplay

--- tpl/config.php
+++ tpl/config.php
@@ -35,6 +35,10 @@ if ( !function_exists( 'add_action' ) ) {
 		<label for="likeit-text"><?php _e('Like-it button text') ?></label>
 		<input type="text" id="likeit-text" name="likeit-text" value="<?php echo get_option('likeit-text')?>" />
 		<br />
+		<label for="likeit-autodisplay"><?php _e('Display button') ?></label>
+		<input type="checkbox" id="likeit-autodisplay" name="likeit-autodisplay" checked="<?php echo get_option('likeit-autodisplay')?>" />
+		<label for="likeit-autodisplay"><?php _e('If checked, the button will be appended to the end of each post. If you want to place it elsewhere, you may use <code>&lt?php likeit_button(); ?&gt;</code> in your theme, but remember that it uses <code>get_the_ID()</code> to get current post\'s id') ?></label>
+		<br />
 		<input type="submit" value="<?php _e('Update options &raquo;')?>" />
 	</form>
 </div>
