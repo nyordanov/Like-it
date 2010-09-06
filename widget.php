17e0237b7be3ebaad9d77f967dab4bf6031e16c6
Nikolay Yordanov <me@nyordanov.com>
2010-09-06 22:00:00 +0300

    add widget.php

---
+++ widget.php
@@ -0,0 +1,17 @@
+<?php
+
+register_widget('Likeit_Widget');
+class Likeit_Widget extends WP_Widget {
+	function Likeit_Widget() {
+	}
+
+	function form($instance) {
+	}
+
+	function update($new_instance, $old_instance) {
+	}
+
+	function widget($args, $instance) {
+	}
+}
+
