3c180aa9b1cf9989e1339b6a64fc6ed75d0110b4
Nikolay Yordanov <me@nyordanov.com>
2010-09-08 11:31:15 +0300

    widget: move config form to tpl/widget-config.php

--- widget.php
+++ widget.php
@@ -11,23 +11,10 @@ class Likeit_Widget extends WP_Widget {
 	}
 
 	function form($instance) {
-		
 		$defaults = array( 'title' => __('Most liked posts'), 'displayed' => '10' );
 		$instance = wp_parse_args( (array) $instance, $defaults );
 		
-		?>
-
-		<p>
-			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e('Title') ?>:</label>
-			<input type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo $instance['title']; ?>" class="widefat" />
-		</p>
-		
-		<p>
-			<label for="<?php echo $this->get_field_id( 'displayed' ); ?>"><?php _e('Number of displayed posts') ?>:</label>
-			<input type="text" id="<?php echo $this->get_field_id( 'displayed' ); ?>" name="<?php echo $this->get_field_name( 'displayed' ); ?>" value="<?php echo $instance['displayed']; ?>" class="widefat" />
-		</p>
-		
-		<?php
+		require_once('tpl/widget-config.php');
 	}
 
 	function update($new_instance, $old_instance) {
